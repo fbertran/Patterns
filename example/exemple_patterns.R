4bc055e11f8d44e1e00d48772975236d46441cfb
theorietsch <theo.rietsch1@gmail.com>
2014-12-10 16:05:11 +0100

    test2

--- example/exemple_patterns.R
+++ example/exemple_patterns.R
@@ -10,13 +10,11 @@ hea_US<-CLL[,which((1:48)%%8<5&(1:48)%%8>0)+2]
 hea_S<-CLL[,which(!((1:48)%%8<5&(1:48)%%8>0))+2]
 
 
-
-
 agg_US<-CLL[,which((1:40)%%8<5&(1:40)%%8>0)+98]
 agg_S<-CLL[,which(!((1:40)%%8<5&(1:40)%%8>0))+98]
 
 
-m_hea_US<-as.micro_array(hea_US,c(60,90,210,390),6,name=CLL[,2],gene_ID=CLL[,1])
+m_hea_US1<-as.micro_array(hea_US,c(60,90,210,390),6,name=CLL[,2],gene_ID=CLL[,1])
 m_hea_S<- as.micro_array(hea_S,c(60,90,210,390),6,name=CLL[,2],gene_ID=CLL[,1])
   
 m_agg_US<-as.micro_array(agg_US,c(60,90,210,390),5,name=CLL[,2],gene_ID=CLL[,1])
@@ -26,7 +24,7 @@ m_agg_S<- as.micro_array(agg_S,c(60,90,210,390),5,name=CLL[,2],gene_ID=CLL[,1])
 selection<-geneSelection(list(m_agg_US,m_agg_S),list("condition",c(1,2)),-1)
 head(selection)
 print(selection)
-
+kfesfl
 
 
 
