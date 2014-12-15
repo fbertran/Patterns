d88ada8910f9a56100bf42f1fa7baf966cf69ce5
nicolasjung <nicolas.julien.jung@gmail.com>
2014-12-15 09:55:39 +0100

    some changes in the example work example use travail.RData

--- example/exemple_patterns.R
+++ example/exemple_patterns.R
@@ -8,8 +8,7 @@ data(CLL)
 require(biomaRt)
 
 affyids=c("202763_at","209310_s_at","207500_at")
-ensembl<-useMart("ensembl")
-
+ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl")
 infos<-getBM(attributes=c("affy_hg_u133_plus_2","ensembl_gene_id","entrezgene","hgnc_symbol","chromosome_name","start_position","end_position","band"),
         filters = "affy_hg_u133_plus_2", values = CLL[,2], mart = ensembl,uniqueRows=TRUE, checkFilters = TRUE)
                  
@@ -29,6 +28,8 @@ m_agg_S<- as.micro_array(agg_S,c(60,90,210,390),5,name=CLL[,2],gene_ID=CLL[,1])
 
 selection<-geneSelection(list(m_agg_US,m_agg_S),list("condition",c(1,2)),-1,alpha=0.1)
 
+print(selection)
+length(selection@gene_ID)
 save.image("travail.RData")
 
 
